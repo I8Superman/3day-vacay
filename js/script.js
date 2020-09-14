0c4202eb36d07185174d1a6138dc38bf2b0918f5
Simon Andersen <simonandersen@Simons-MacBook-Air-2.local>
2020-09-14 15:59:47 +0200

    Geting the data, initial commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+// Getting the data from Google Sheets
 
+const datalink = "http://spreadsheets.google.com/feeds/list/18QShemZlLoq2j6zasY3bNjFkoqkDb_tP7Tpjujma3mg/od6/public/values?alt=json";
+
+window.addEventListener("DOMContentLoaded", getData);
+
+function getData(){
+    fetch(datalink)
+    .then(response => response.json())
+    .then(handleData);
+}
+
+function handleData(data) {
+    const myData = data.feed.entry;
+    console.log("Here is our data");
+    console.log(myData);
+    myData.forEach(showData);
+}
+
+function showData(singleActivity) {
+    console.log(singleActivity.gsx$activitytype.$t);
+}
